823a888051b8c412286faec2e2f8b529f3db6457
mahanaoishnd <153020689+mahanaoishnd@users.noreply.github.com>
2025-12-11 19:37:30 -0300

    Update listanoticias.js

--- src/pages/listanoticias.js
+++ src/pages/listanoticias.js
@@ -2,7 +2,7 @@ import { Container, Row, Table } from 'react-bootstrap';
 import Tablelist from './components/table';
 export async function getServerSideProps() {
   // Fetch data from external API
-  const res = await fetch('https://api6anoticias.vercel.app/noticias')
+  const res = await fetch('https://fwapi-geovana.vercel.app/noticias')
   const repo = await res.json()
   // Pass data to the page via props
   return { props: { noticias: repo } }
